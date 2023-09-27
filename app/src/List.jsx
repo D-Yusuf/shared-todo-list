0f314a2a2e1080d35f0730b62185ccf84d17a65b
Yusuf Alnasiri <alnasiriyusuf@gmail.com>
2023-09-27 19:06:19 +0300

    trying

--- app/src/List.jsx
+++ app/src/List.jsx
@@ -5,9 +5,6 @@ import { useEffect } from "react";
 
 export default function List() {
   let dataItems = data.map(item =>  <Item key={item.id} task={item.task} done={item.done} />)
-    useEffect(()=>{
-      dataItems = data.map(item =>  <Item key={item.id} task={item.task} done={item.done} />);
-    })
   return (
     <div className="flex flex-col items-center overflow-y-auto h-60">
         {dataItems}
@@ -15,3 +12,4 @@ export default function List() {
   );
   }
 
+// TO BE CONTINUED
